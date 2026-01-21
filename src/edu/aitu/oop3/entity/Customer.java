643948c0879def520d2d00ad8541e0f8b33738c0
iamadolphinn <dnrskiii@gmail.com>
2026-01-21 11:28:55 +0500

    Daniyar is adding the Customer and MenuItem classes

--- src/edu/aitu/oop3/entity/Customer.java
+++ src/edu/aitu/oop3/entity/Customer.java
@@ -1,4 +1,19 @@
 package edu.aitu.oop3.entity;
 
 public class Customer {
+    private int id;
+    private String name;
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    protected int getId() {
+        return id;
+    }
+
+    protected String getName() {
+        return name;
+    }
 }
--- src/edu/aitu/oop3/entity/MenuItem.java
+++ src/edu/aitu/oop3/entity/MenuItem.java
@@ -1,4 +1,31 @@
 package edu.aitu.oop3.entity;
 
 public class MenuItem {
+    private int id;
+    private String name;
+    private double price;
+    private boolean available;
+
+    public MenuItem(int id, String name, double price, boolean available) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.available = available;
+    }
+
+    protected int getId() {
+        return id;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected double getPrice() {
+        return price;
+    }
+
+    protected boolean isAvailable() {
+        return available;
+    }
 }
