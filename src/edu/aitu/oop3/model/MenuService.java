49485742c27fa37c936509a5696a95d028985f5a
Adilet <bstop636@gmail.com>
2026-01-24 14:50:04 +0500

    Duisekenov Adilet, i adding and updated menu and order service

--- src/edu/aitu/oop3/model/MenuService.java
+++ src/edu/aitu/oop3/model/MenuService.java
@@ -1,4 +1,35 @@
 package edu.aitu.oop3.model;
 
+import edu.aitu.oop3.exception.MenuItemNotAvailableException;
+
+import java.util.List;
+
 public class MenuService {
+
+    private MenuItemRepository menuRepo;
+
+    public MenuService(MenuItemRepository menuRepo) {
+        this.menuRepo = menuRepo;
+    }
+
+    public List<MenuItem> getMenu() {
+        return menuRepo.findAll();
+    }
+
+    public List<MenuItem> getAvailableMenu() {
+        return menuRepo.findAvailable();
+    }
+
+    public MenuItem requireAvailable(int menuItemId) {
+        MenuItem item = menuRepo.findById(menuItemId);
+
+        if (item == null) {
+            throw new MenuItemNotAvailableException("Menu item not found: " + menuItemId);
+        }
+        if (!item.available) {
+            throw new MenuItemNotAvailableException("Menu item is not available: " + item.name);
+        }
+        return item;
+    }
 }
+
--- src/edu/aitu/oop3/model/OrderService.java
+++ src/edu/aitu/oop3/model/OrderService.java
@@ -1,4 +1,42 @@
 package edu.aitu.oop3.model;
 
+import edu.aitu.oop3.exception.InvalidQuantityException;
+
+import java.util.List;
+
 public class OrderService {
+
+    private OrderRepository orderRepo;
+    private MenuService menuService;
+    private CustomerRepository customerRepo;
+
+    public OrderService(OrderRepository orderRepo, MenuService menuService, CustomerRepository customerRepo) {
+        this.orderRepo = orderRepo;
+        this.menuService = menuService;
+        this.customerRepo = customerRepo;
+    }
+
+    public Order placeOrder(int customerId, int menuItemId, int quantity) {
+        if (quantity <= 0) {
+            throw new InvalidQuantityException("Quantity must be > 0");
+        }
+
+        customerRepo.findById(customerId);
+
+        MenuItem item = menuService.requireAvailable(menuItemId);
+
+        Order order = orderRepo.createOrder(customerId);
+        orderRepo.addItem(order.id, item.id, quantity, item.price);
+
+        return order;
+    }
+
+    public List<Order> viewActiveOrders() {
+        return orderRepo.findActiveOrders();
+    }
+
+    public void completeOrder(int orderId) {
+        orderRepo.markCompleted(orderId);
+    }
 }
+
