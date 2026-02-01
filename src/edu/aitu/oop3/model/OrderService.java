55d83621ff6c1fb3ded61fa1d7c28271ed381fa9
iamadolphinn <dnrskiii@gmail.com>
2026-02-01 14:47:29 +0500

    Daniyar is updating the previous OrderService

--- src/edu/aitu/oop3/model/OrderService.java
+++ src/edu/aitu/oop3/model/OrderService.java
@@ -1,6 +1,10 @@
 package edu.aitu.oop3.model;
 
 import edu.aitu.oop3.exception.InvalidQuantityException;
+import edu.aitu.oop3.pattern.builder.PlaceOrderRequest;
+import edu.aitu.oop3.pattern.factory.DeliveryFactory;
+import edu.aitu.oop3.pattern.factory.DeliveryOption;
+import edu.aitu.oop3.pattern.singleton.TaxConfig;
 
 import java.util.List;
 
@@ -17,17 +21,40 @@ public class OrderService {
     }
 
     public Order placeOrder(int customerId, int menuItemId, int quantity) {
-        if (quantity <= 0) {
-            throw new InvalidQuantityException(quantity);
-
-        }
+        if (quantity <= 0) throw new InvalidQuantityException(quantity);
 
         customerRepo.findById(customerId);
-
         MenuItem item = menuService.requireAvailable(menuItemId);
 
         Order order = orderRepo.createOrder(customerId);
         orderRepo.addItem(order.id, item.id, quantity, item.price);
+
+        return order;
+    }
+
+    public Order placeOrder(PlaceOrderRequest req) {
+        if (req.getQuantity() <= 0) throw new InvalidQuantityException(req.getQuantity());
+
+        customerRepo.findById(req.getCustomerId());
+        MenuItem item = menuService.requireAvailable(req.getMenuItemId());
+
+        DeliveryOption opt = DeliveryFactory.create(req.getDeliveryType(), req.getAddress());
+
+        double subtotal = item.price * req.getQuantity();
+
+        double fee = opt.fee(subtotal);
+
+        double totalWithFee = subtotal + fee;
+        double totalWithTax = TaxConfig.getInstance().addTax(totalWithFee);
+
+        Order order = orderRepo.createOrder(req.getCustomerId());
+        orderRepo.addItem(order.id, item.id, req.getQuantity(), item.price);
+
+        System.out.println("Delivery type: " + opt.getType());
+        System.out.println("Subtotal: " + subtotal);
+        System.out.println("Fee: " + fee);
+        System.out.println("Tax%: " + TaxConfig.getInstance().getTaxPercent());
+        System.out.println("Total: " + totalWithTax);
 
         return order;
     }
@@ -40,4 +67,3 @@ public class OrderService {
         orderRepo.markCompleted(orderId);
     }
 }
-
