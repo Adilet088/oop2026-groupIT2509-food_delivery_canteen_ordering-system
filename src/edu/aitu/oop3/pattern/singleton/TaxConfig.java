86a1511e5b74706f427f7b71658598528e26e948
iamadolphinn <dnrskiii@gmail.com>
2026-02-01 14:34:07 +0500

    Daniyar is adding the TaxConfig class

--- src/edu/aitu/oop3/pattern/singleton/TaxConfig.java
+++ src/edu/aitu/oop3/pattern/singleton/TaxConfig.java
@@ -1,4 +1,27 @@
 package edu.aitu.oop3.pattern.singleton;
 
-public class TaxConfig {
+public final class TaxConfig {
+    private static final TaxConfig INSTANCE = new TaxConfig();
+
+
+    private double taxPercent = 12.0;
+
+    private TaxConfig() {}
+
+    public static TaxConfig getInstance() {
+        return INSTANCE;
+    }
+
+    public double getTaxPercent() {
+        return taxPercent;
+    }
+
+    public void setTaxPercent(double taxPercent) {
+        if (taxPercent < 0) taxPercent = 0;
+        this.taxPercent = taxPercent;
+    }
+
+    public double addTax(double amount) {
+        return amount + (amount * taxPercent / 100.0);
+    }
 }
